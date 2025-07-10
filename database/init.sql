4d8f9b6762cbf0a53f6a4faf4c6b3cba2441f9b3
fede1817 <frayg52@gmail.com>
2025-07-10 11:51:38 -0300

    base de datos actualizada

--- database/init.sql
+++ database/init.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2025-06-30 15:15:41
+-- Started on 2025-07-10 11:49:46
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -34,7 +34,11 @@ CREATE TABLE public.impresoras (
     sucursal character varying(100),
     modelo character varying(100),
     drivers_url text,
-    tipo character varying(50)
+    tipo character varying(50),
+    fecha_ultimo_cambio timestamp without time zone,
+    cambios_toner integer DEFAULT 0,
+    toner_reserva integer DEFAULT 0,
+    toner_anterior integer DEFAULT 0
 );
 
 
@@ -57,7 +61,7 @@ CREATE SEQUENCE public.impresoras_id_seq
 ALTER SEQUENCE public.impresoras_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4796 (class 0 OID 0)
+-- TOC entry 4799 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: impresoras_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -74,43 +78,43 @@ ALTER TABLE ONLY public.impresoras ALTER COLUMN id SET DEFAULT nextval('public.i
 
 
 --
--- TOC entry 4790 (class 0 OID 16440)
+-- TOC entry 4793 (class 0 OID 16440)
 -- Dependencies: 218
 -- Data for Name: impresoras; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.impresoras (id, ip, sucursal, modelo, drivers_url, tipo) FROM stdin;
-10	192.168.48.121	Concepcion	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
-2	192.168.5.21	Pedro Juan Caballero	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
-3	192.168.4.21	Caaguazu	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
-4	192.168.3.21	Ciudad del Este	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
-5	192.168.2.21	Encarnacion	SP 8400DN	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343271/V3100/z03651L16.exe	principal
-6	192.168.8.23	Asuncion	IM 430	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343273/V3100/z03655L16.exe	principal
-7	192.168.8.20	Asuncion-color	P C600	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343154/V3200/z04340L16.exe	principal
-8	192.168.8.41	Asuncion-RRHH	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	principal
-9	192.168.46.21	Misiones	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
-11	192.168.7.22	Santani	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup
-12	192.168.5.22	Pedro Juan Caballero	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup
-13	192.168.4.22	Caaguazu	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup
-14	192.168.3.22	Ciudad del Este	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup
-17	192.168.48.122	Concepcion	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup
-15	192.168.2.22	Encarnacion	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup
-16	192.168.46.22	Misiones	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup
-1	192.168.7.21	Santani	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal
+COPY public.impresoras (id, ip, sucursal, modelo, drivers_url, tipo, fecha_ultimo_cambio, cambios_toner, toner_reserva, toner_anterior) FROM stdin;
+13	192.168.4.22	Caaguazu	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup	2025-07-01 14:32:49.048432	1	1	30
+17	192.168.48.122	Concepcion	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup	2025-07-01 14:32:50.027286	1	1	30
+14	192.168.3.22	Ciudad del Este	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup	\N	0	0	-2
+4	192.168.3.21	Ciudad del Este	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-01 14:32:48.966755	1	1	60
+6	192.168.8.23	Asuncion	IM 430	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343273/V3100/z03655L16.exe	principal	2025-07-01 14:32:48.985148	1	1	80
+8	192.168.8.41	Asuncion-RRHH	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	principal	2025-07-01 14:32:48.995952	1	1	40
+7	192.168.8.20	Asuncion-color	P C600	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343154/V3200/z04340L16.exe	principal	2025-07-01 14:32:48.99016	1	1	80
+15	192.168.2.22	Encarnacion	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup	\N	0	0	0
+16	192.168.46.22	Misiones	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	backup	\N	0	0	0
+3	192.168.4.21	Caaguazu	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-03 10:26:24.882666	2	1	80
+5	192.168.2.21	Encarnacion	SP 8400DN	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343271/V3100/z03651L16.exe	principal	2025-07-01 14:32:48.979574	1	1	60
+10	192.168.48.121	Concepcion	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-03 11:26:37.00585	2	1	90
+9	192.168.46.21	Misiones	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-01 14:32:49.00689	1	1	60
+2	192.168.5.21	Pedro Juan Caballero	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-01 14:32:47.976952	1	1	40
+1	192.168.7.21	Santani	RICOH P 800	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343146/V3200/z04344L17.exe	principal	2025-07-01 14:32:58.087463	1	1	30
+11	192.168.7.22	Santani	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup	2025-07-01 14:32:49.017411	1	1	30
+12	192.168.5.22	Pedro Juan Caballero	P 501/502	https://support.ricoh.com/bb/pub_e/dr_ut_e/0001343/0001343272/V3100/z03653L16.exe	backup	2025-07-01 14:32:49.03102	1	1	90
 \.
 
 
 --
--- TOC entry 4797 (class 0 OID 0)
+-- TOC entry 4800 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: impresoras_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.impresoras_id_seq', 27, true);
+SELECT pg_catalog.setval('public.impresoras_id_seq', 29, true);
 
 
 --
--- TOC entry 4643 (class 2606 OID 16447)
+-- TOC entry 4646 (class 2606 OID 16447)
 -- Name: impresoras impresoras_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -118,7 +122,7 @@ ALTER TABLE ONLY public.impresoras
     ADD CONSTRAINT impresoras_pkey PRIMARY KEY (id);
 
 
--- Completed on 2025-06-30 15:15:41
+-- Completed on 2025-07-10 11:49:46
 
 --
 -- PostgreSQL database dump complete
